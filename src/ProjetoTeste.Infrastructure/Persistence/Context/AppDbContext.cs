0e3fbeb88fc8aede90f66bc22de646374869afb4
José Henrique <josehenriquefernandes346@gmail.com>
2024-12-26 14:27:10 -0300

    Fixing Errors on Mapping

--- src/ProjetoTeste.Infrastructure/Persistence/Context/AppDbContext.cs
+++ src/ProjetoTeste.Infrastructure/Persistence/Context/AppDbContext.cs
@@ -9,6 +9,7 @@ namespace ProjetoTeste.Infrastructure.Persistence.Context
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Client> Clients { get; set; }
+        public DbSet<Brand> Brands { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
